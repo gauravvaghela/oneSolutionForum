7f0c9b49a76bb8943fc33f3ab54ecee453916a9e
gauravvaghela <vaghelagaura007@gmail.com>
2017-09-14 01:54:38 +0530

    UserModel Class

---
+++ oneSolutionForum.Core/ViewModel/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oneSolutionForum.Core.ViewModel
+{
+    public class UserModel
+    {
+
+    }
+}
